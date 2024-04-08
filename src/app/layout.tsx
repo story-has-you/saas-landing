cdce945bdb2ccd34f85f3021d6a01efddff9d51b
fangxi <loveinori@qq.com>
2024-04-08 13:25:09 +0800

    不用的组件。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
 import { buildMetadata, buildViewport, siteConfig } from "@/config/site";
-import "@/styles/globals.css";
 import { fontSans } from "@/lib/fonts";
-import { GoogleAnalytics } from "@next/third-parties/google";
 import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 import { LayoutProps } from "@/types/layout";
-import { ThemeProvider } from "@/components/theme-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = buildMetadata();
 export const viewport = buildViewport();
@@ -13,9 +12,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background antialiased", fontSans.className)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <main className="flex-1">{children}</main>
-        </ThemeProvider>
+        <main className="flex-1">{children}</main>
       </body>
       {siteConfig.useGoogleAnalytics && <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID!} />}
     </html>
