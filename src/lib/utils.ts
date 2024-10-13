9e873709c243413b7760f15bdc4476e7d9cb45b8
fangxi <loveinori@qq.com>
2024-10-13 15:45:19 +0800

    3

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
-import { customAlphabet } from "nanoid";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const nanoid = customAlphabet("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", 10);
